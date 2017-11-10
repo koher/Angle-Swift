c2f05e9f9d2b7042162a8ceab6e8bcba0ca180fb
Yuta Koshizawa <koher@koherent.org>
2017-11-10 10:44:05 +0900

    Recreate xcodeproj for iOS

---
+++ Sources/Angle/Angle.h
@@ -0,0 +1,19 @@
+//
+//  Angle.h
+//  Angle
+//
+//  Created by Yuta Koshizawa on 2017/11/10.
+//  Copyright © 2017 koherent.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Angle.
+FOUNDATION_EXPORT double AngleVersionNumber;
+
+//! Project version string for Angle.
+FOUNDATION_EXPORT const unsigned char AngleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Angle/PublicHeader.h>
+
+
